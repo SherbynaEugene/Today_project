3a93704b32e3163ebb225e24a968139504c41612
Yuliana <iyunanext@gmail.com>
2026-02-21 09:05:35 +0200

    Update urls.py
    updated

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = "tasks"
-
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('today/', views.today_tasks, name='today_tasks'),
     path('add/', views.add_task, name='add_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
